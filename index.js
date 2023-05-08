614464fb347fca4a429582060844ca585f931c11
BarelyAnXer <christiangazzingan38@gmail.com>
2023-05-08 20:21:09 +0800

    fixing cors

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const cors = require('cors');
-// const path = require('path');
-
 const PORT = process.env.PORT || 5000;
-
 const app = express();
-
-// Connect to database
 connectDB();
 
 app.use(express.json());
 app.use(cors());
 
-// Serve static files of React app
-// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://relaxed-profiterole-764f54.netlify.app');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
-// Define API routes
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
-
-// Handle all other requests
-// app.get('*', (req, res) => {
-//   console.log(path.join(__dirname, 'frontend/dist', 'index.html'))
-//   res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
-// });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
