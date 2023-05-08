aa53c0216ff60cd48ad8530a8e04737491f32bf5
BarelyAnXer <christiangazzingan38@gmail.com>
2023-05-08 20:09:41 +0800

    added footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import styles from "./Footer.module.css" 
 function Footer() {
   return (
     <>
-
+ <footer>
+    <p>&copy; 2023 Christian Cabral Gazzingan </p>
+  </footer>
     </>
   );
 }
