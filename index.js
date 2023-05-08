be6e8e75197f4474753fc8b6f0aa39f72e449c23
BarelyAnXer <christiangazzingan38@gmail.com>
2023-05-08 20:41:27 +0800

    cors parin

--- index.js
+++ index.js
@@ -6,11 +6,17 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors({
-  origin: 'https://relaxed-profiterole-764f54.netlify.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+// app.use(cors({
+//   origin: 'https://relaxed-profiterole-764f54.netlify.app',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://relaxed-profiterole-764f54.netlify.app"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', require('./routes/index'));
 app.use('/api/url', require('./routes/url'));
