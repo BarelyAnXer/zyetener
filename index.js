9b92f549283326e1351f0c5c93086289a8179125
BarelyAnXer <christiangazzingan38@gmail.com>
2023-05-09 09:48:51 +0800

    fix cors

--- index.js
+++ index.js
@@ -6,17 +6,12 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors({
-  origin: 'https://relaxed-profiterole-764f54.netlify.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(cors());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://relaxed-profiterole-764f54.netlify.app');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, PUT, PATCH, GET, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 });
 
